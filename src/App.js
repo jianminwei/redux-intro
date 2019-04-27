aa7f603b0998509eafc8040e4a6297db2192ff2b
Jianmin <jianminwei1@gmail.com>
2019-04-27 09:19:50 -0400

    Added INCREASE_COUNTER event and the event handler in the reducer

--- src/App.js
+++ src/App.js
@@ -7,12 +7,22 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    //give the initial counter 0
+    //give the initial props counter 0
     this.props = {counter: 0};
+
+    //bind the methods as a good practice
+    this.onButtonClick.bind(this);
   }
 
   onButtonClick = e => {
-    //this.setState({ counter: this.state.counter + 1 });
+      //1. This dispatch is injected by react-redux connect
+      //2. Dispatch a action, and send the payload.
+      this.props.dispatch({ 
+        type: 'INCREASE_COUNTER',
+        payload: {
+          counter_increment_unit: 2
+        } 
+      })   
   };
 
   render() {
@@ -31,6 +41,8 @@ class App extends Component {
   }
 }
 
+//This mapStateToProps function maps redux "dispatch" function
+//in addition to state as properties.
 function mapStateToProps(state) {
   return {
     counter: state.counter
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,11 @@
 
 export  function counter(state = {counter: 5}, action) {
+
+    //action handler can only derive a new state based on payload.
+    //It can't mutate the existing previous state.
+    if (action.type === 'INCREASE_COUNTER') { 
+        return { counter: state.counter + action.payload.counter_increment_unit };
+    }
+
     return state
 }
