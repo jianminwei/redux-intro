92ffded4b0fe777a8127d2f05945e31e7966ae9f
Jianmin <jianminwei1@gmail.com>
2019-04-27 06:40:20 -0400

    A simple update counter by button example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 5
+    };
+  }
+
+  onButtonClick = e => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  render() {
+    return (
+      <div className="redux-self-app">
+        <header className="header">
+          <h2>Redux-self</h2>
+        </header>
+        <div>
+          Counter: <span id="counter">{this.state.counter}</span>
+        </div>
+        
+        <button onClick={this.onButtonClick}>Increase Counter</button>
+      </div>
+    );
+  }
 }
 
 export default App;
